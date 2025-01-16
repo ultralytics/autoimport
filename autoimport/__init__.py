eaaa2fed055bf8b0030a95b98ceffd8a6609fbd4
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-16 15:58:14 +0100

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- autoimport/__init__.py
+++ autoimport/__init__.py
@@ -1,7 +1,6 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-__version__ = "0.0.0"
-
 from autoimport.main import LazyLoader, lazy
 
 __all__ = ("LazyLoader", "lazy")
+__version__ = "0.0.0"
