03092e4b8c523950a4d3d45feb54a84058eb064f
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-16 15:26:21 +0100

    Update pyproject.toml (#2)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,6 +1,6 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-from template.module1 import add_numbers
+from autoimport.module1 import add_numbers
 
 
 def test_add_numbers():
