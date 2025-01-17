41b24cdc9e645880336c7ee9028db9275d235609
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-17 14:21:21 +0100

    Repr updates

--- autoimport/main.py
+++ autoimport/main.py
@@ -22,7 +22,9 @@ class LazyLoader(types.ModuleType):
             self._loading = True  # Set loading flag
             try:
                 self._module = importlib.import_module(self.__name__)
-                # self.__class__ = self._module.__class__  # Change the class to the actual module's class
+                for attr in ['__file__', '__path__', '__package__', '__spec__', '__class__']:
+                    if hasattr(self._module, attr):
+                        setattr(self, attr, getattr(self._module, attr))
             finally:
                 self._loading = False  # Reset loading flag
 
